ca79d27ea52728910570c648f045e303b1c1af91
Jay Quigley & Matt Bogen <pair+jayqui.zebogen@devbootcamp.com>
2015-08-19 13:17:41 -0500

    incorporate third-party solution (markdown.js)

--- models/markdown_parser.js
+++ models/markdown_parser.js
@@ -4,19 +4,23 @@ function Parser(){
 }
 
 Parser.prototype.parse = function(text) {
-  // parsing logic
-  var outputText = text.replace(/(\s|\A|^|>)\*{2}(.+)\*{2}(\s|\z|$|<)/m, this.boldText);
-  outputText = outputText.replace(/(\s|\A|^|>)\*(.+)\*(\s|\z|$|<)/m, this.italicText);
-  outputText = outputText.replace(/(\s|\A|^|>)_(.+)_(\s|\z|$|<)/m, this.italicText);
-  console.log(outputText);
-  return outputText;
+
+  // markdown-js's parsing logic
+  return markdown.toHTML(text);
+
+  // our parsing logic
+//   var outputText = text.replace(/(\s|\A|^|>)\*{2}(.+)\*{2}(\s|\z|$|<)/m, this.boldText);
+//   outputText = outputText.replace(/(\s|\A|^|>)\*(.+)\*(\s|\z|$|<)/m, this.italicText);
+//   outputText = outputText.replace(/(\s|\A|^|>)_(.+)_(\s|\z|$|<)/m, this.italicText);
+//   console.log(outputText);
+//   return outputText;
 }
 
-Parser.prototype.boldText = function(match, separator1, boldString, separator2) {
-  return separator1 + "<span class='md-bold'>" + boldString + "</span>" + separator2;
-}
+// Parser.prototype.boldText = function(match, separator1, boldString, separator2) {
+//   return separator1 + "<span class='md-bold'>" + boldString + "</span>" + separator2;
+// }
 
-Parser.prototype.italicText = function(match, separator1, italicString, separator2) {
-  return separator1 + "<span class='md-italic'>" + italicString + "</span>" + separator2;
-}
+// Parser.prototype.italicText = function(match, separator1, italicString, separator2) {
+//   return separator1 + "<span class='md-italic'>" + italicString + "</span>" + separator2;
+// }
 
