b4171b52e1b2bd88e646555d9a7cf12aca172322
Jay Quigley & Matt Bogen <pair+jayqui.zebogen@devbootcamp.com>
2015-08-19 13:09:14 -0500

    Add rest of release 1 functionality

--- models/markdown_parser.js
+++ models/markdown_parser.js
@@ -1,13 +1,22 @@
+//** hello ** this is **weird isn't it**
+
 function Parser(){
 }
 
 Parser.prototype.parse = function(text) {
   // parsing logic
-  var outputText = text.replace(/\*\*(\w+)\*\*/, this.boldText);
+  var outputText = text.replace(/(\s|\A|^|>)\*{2}(.+)\*{2}(\s|\z|$|<)/m, this.boldText);
+  outputText = outputText.replace(/(\s|\A|^|>)\*(.+)\*(\s|\z|$|<)/m, this.italicText);
+  outputText = outputText.replace(/(\s|\A|^|>)_(.+)_(\s|\z|$|<)/m, this.italicText);
   console.log(outputText);
   return outputText;
 }
 
-Parser.prototype.boldText = function(match, boldString) {
-  return "<span class='md-bold'>" + boldString + "</span>";
+Parser.prototype.boldText = function(match, separator1, boldString, separator2) {
+  return separator1 + "<span class='md-bold'>" + boldString + "</span>" + separator2;
 }
+
+Parser.prototype.italicText = function(match, separator1, italicString, separator2) {
+  return separator1 + "<span class='md-italic'>" + italicString + "</span>" + separator2;
+}
+
