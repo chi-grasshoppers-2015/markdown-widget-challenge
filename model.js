d7a72ed0b80090db33d3d57b2f466a33781d1a25
Jasmine Feldmann & Nicholas Kincaid <pair+jasmine-feldmann.nbkincaid@devbootcamp.com>
2015-08-19 12:40:37 -0500

    add working functionality for more than one word

--- model.js
+++ model.js
@@ -11,22 +11,39 @@ Markdown.prototype.transform = function(content) {
 
   var strings = content.split("\n");
   for (var i = 0; i < strings.length; i++) {
-    var strings_row = strings[i].split(" ")
-    for (var j = 0; j < strings_row.length; j++) {
-      var newText = this.wordTransform(strings_row[j])
-      strings_row[j] = newText;
-    }
-    strings[i] = strings_row.join(" ");
+    // var strings_row = strings[i].split(" ")
+    // for (var j = 0; j < strings_row.length; j++) {
+    //   var newText = this.wordTransform(strings_row[j])
+    //   strings_row[j] = newText;
+    // }
+    // strings[i] = strings_row.join(" ");
+    newText = this.rowTransform(strings[i]);
+    strings[i] = newText;
   };
   return strings
 };
 
-Markdown.prototype.wordTransform = function(rawWord) {
-  if (rawWord.match(/[*].*[*]/) != null) {
-    var newWord = rawWord.replace(/[*]/g, "");
-    return "<em>" + newWord + "</em>"
+Markdown.prototype.rowTransform = function(rawRow) {
+  if (rawRow.match(/[*]{2}.+?[*]{2}/) != null) {
+    var matchedWord = (/[*]{2}.+?[*]{2}/g).exec(rawRow);
+    var start_index = matchedWord.index + 2
+    var end_index = matchedWord[0].length - 2
+    var newRow = rawRow.replace(matchedWord[0], "<strong>" + matchedWord[0].slice(start_index, end_index) + "</strong>")
+    return newRow
+  }
+  else if (rawRow.match(/[*].+?[*]/) != null) {
+    var matchedWord = (/[*].+?[*]/g).exec(rawRow);
+    var start_index = matchedWord.index + 1
+    var end_index = matchedWord[0].length - 1
+    var newRow = rawRow.replace(matchedWord[0], "<em>" + matchedWord[0].slice(start_index, end_index) + "</em>")
+  }
+  else if (rawRow.match(/[_].+?[_]/) != null) {
+    var matchedWord = (/[_].+?[_]/g).exec(rawRow);
+    var start_index = matchedWord.index + 1
+    var end_index = matchedWord[0].length - 1
+    var newRow = rawRow.replace(matchedWord[0], "<em>" + matchedWord[0].slice(start_index, end_index) + "</em>")
   }
   else {
-    return rawWord
+    return rawRow
   }
 };
